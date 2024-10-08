b70bfe7d47bafb10a22452da968f96777840a7ab
Alexzander19 <2961781@mail.ru>
2024-10-08 21:04:43 +0700

    Update main.py
    Задача 3. Студент тоже человек

--- main.py
+++ main.py
@@ -75,3 +75,37 @@ except ValueError as e:
     print(e,'Попробуйте с другой суммой')
 
 
+# Задача 3: Наследование
+
+# Описание: Создайте класс Person, который будет хранить информацию о человеке (имя и возраст), 
+# и класс Student, который наследуется от Person. В классе Student должны быть добавлены поля 
+# для хранения учебного заведения и среднего балла.
+
+# Условия:
+
+#  • Конструктор класса Person должен принимать имя и возраст.
+#  • Конструктор класса Student должен дополнительно принимать учебное заведение и средний балл.
+#  • Добавьте методы для отображения информации о студенте.
+
+
+class Person:
+    def __init__(self,name: str, age: int):
+        self._name: str = name
+        self._age: int = age
+
+    def __str__(self) -> str:
+        return (f'ИМЯ: {self._name}, возраст: {self._age}')
+
+class Student(Person):
+    def __init__(self, name: str, age: int, institution: str, score: float):
+        super().__init__(name, age)
+        self.__institution: str = institution
+        self.__score: float = score
+
+    def __str__(self) -> str:
+        return super().__str__() + f' Учится в {self.__institution}, средний балл {self.__score}'
+    
+
+s = Student('Иванов',21,'СФУ',4.2)
+
+print(s)
